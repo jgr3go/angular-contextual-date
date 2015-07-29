ee3837b13b2657d5186b3fc2ea9ce47891721bf3
Jon Gregorowicz <jonathan.gregorowicz@gmail.com>
2015-07-29 16:18:01 -0400

    Adding karma-coverage to tests

--- karma.conf.js
+++ karma.conf.js
@@ -33,16 +33,22 @@ module.exports = function(config) {
     coverage: {
     },
 
+    coverageReporter: {
+        type: 'html',
+        dir: 'coverage/'
+    },
+
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+        'src/*.js': ['coverage']
     },
 
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['dots', 'coverage'],
 
 
     // web server port
